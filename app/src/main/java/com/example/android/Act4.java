1a4eb31f01195d97c4580722f444de0de15f1832
MalCaor <xav.lemen@orange.fr>
2019-12-06 09:44:28 +0100

    erreur act 4 catch

--- app/src/main/java/com/example/android/Act4.java
+++ app/src/main/java/com/example/android/Act4.java
@@ -7,6 +7,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.content.Intent;
+import android.widget.Toast;
 
 
 public class Act4 extends AppCompatActivity {
@@ -18,18 +19,31 @@ public class Act4 extends AppCompatActivity {
         Button fin=(Button) findViewById(R.id.btnOk);
         fin.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                boolean errorAge = false;
                 String age=((EditText)findViewById(R.id.etAge)).getText().toString();
-                int iage=Integer.parseInt(age);
-                String numf=((EditText)findViewById(R.id.etNum)).getText().toString();
-                long lfetiche=Long.parseLong(numf);
-                Intent intent = new Intent ();
-                intent.putExtra ("string_1", ((EditText)findViewById(R.id.etNom)).getText().toString());
-                intent.putExtra ("string_2", ((EditText)findViewById(R.id.etPre)).getText().toString());
-                intent.putExtra ("int_1", iage);
-                intent.putExtra ("long_1", lfetiche);
+                try
+                {
+                    // check age
+                    int iage=Integer.parseInt(age);
+                }
+                catch (NumberFormatException e)
+                {
+                    Toast.makeText(getApplicationContext(), "Age Invalide", Toast.LENGTH_SHORT).show();
+                    errorAge = true;
+                }
+                if (!errorAge) {
+                    int iage=Integer.parseInt(age);
+                    String numf = ((EditText) findViewById(R.id.etNum)).getText().toString();
+                    long lfetiche = Long.parseLong(numf);
+                    Intent intent = new Intent();
+                    intent.putExtra("string_1", ((EditText) findViewById(R.id.etNom)).getText().toString());
+                    intent.putExtra("string_2", ((EditText) findViewById(R.id.etPre)).getText().toString());
+                    intent.putExtra("int_1", iage);
+                    intent.putExtra("long_1", lfetiche);
 
-                setResult (Activity.RESULT_OK,intent);
-                finish();
+                    setResult(Activity.RESULT_OK, intent);
+                    finish();
+                }
 
             }
         });
