6e04279c76d10ee70c8c19002748a6684ac3ae42
MalCaor <xav.lemen@orange.fr>
2019-12-10 17:01:21 +0100

    try catch de ses mort

--- app/src/main/java/com/example/android/Activity5.java
+++ app/src/main/java/com/example/android/Activity5.java
@@ -3,6 +3,9 @@ package com.example.android;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.text.ParseException;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
@@ -25,7 +28,7 @@ public class Activity5 extends AppCompatActivity {
         String mapref = myPrefs.getString("mapref", "nothing");
         JsonElement vjson = new JsonParser().parse(mapref);
         Gson gson = new GsonBuilder().setDateFormat("dd/MM/yyyy").create();
-        eleve vmapref=gson.fromJson(vjson,eleve.class);
+        final eleve vmapref=gson.fromJson(vjson,eleve.class);
         Toast.makeText(getApplicationContext(),    vmapref.getNom()+" "+vmapref.getPrenom()+" "+new SimpleDateFormat("dd/MM/yyyy").format(vmapref.getDatenaiss()) ,Toast.LENGTH_LONG).show();
 
         // set the TextView
@@ -58,13 +61,94 @@ public class Activity5 extends AppCompatActivity {
 
         // OK btn
         Ok.setOnClickListener(new View.OnClickListener() {
-            boolean BNum, BNom, BPre, BAge, BAdd, BDate;
-            BNum = BNom = BPre = BAge = BAdd = BDate = false;
             public void onClick(View v) {
+
+                // les var de test
+                boolean BNum, BNom, BPre, BAge, BAdd, BDate;
+                BNum = BNom = BPre = BAge = BAdd = BDate = false;
+
+
+                // ### LES TESTS ###
+                // LE NUM
                 try {
-                    String num=((EditText)findViewById(R.id.etNum)).getText().toString();
+                    int num=Integer.parseInt(((EditText)findViewById(R.id.etNum)).getText().toString());
+                }
+                catch (Exception e){
+                    Toast.makeText(getApplicationContext(), "Num invalide", Toast.LENGTH_SHORT).show();
+                    BNum = true;
                 }
 
+                // LE NOM
+                try {
+                    String nom=((EditText)findViewById(R.id.etNom)).getText().toString();
+                }
+                catch (Exception e){
+                    Toast.makeText(getApplicationContext(), "Nom invalide", Toast.LENGTH_SHORT).show();
+                    BNom = true;
+                }
+
+                // LE PRENOM
+                try {
+                    String pre=((EditText)findViewById(R.id.etPrenom)).getText().toString();
+                }
+                catch (Exception e){
+                    Toast.makeText(getApplicationContext(), "Prenom invalide", Toast.LENGTH_SHORT).show();
+                    BPre = true;
+                }
+
+                //L AGE
+                try {
+                    int age=Integer.parseInt(((EditText)findViewById(R.id.etAge)).getText().toString());
+                }
+                catch (Exception e){
+                    Toast.makeText(getApplicationContext(), "Age invalide", Toast.LENGTH_SHORT).show();
+                    BAge = true;
+                }
+
+                // L ADRESS
+                try {
+                    String Add=((EditText)findViewById(R.id.etAdd)).getText().toString();
+                }
+                catch (Exception e){
+                    Toast.makeText(getApplicationContext(), "Adress invalide", Toast.LENGTH_SHORT).show();
+                    BAdd = true;
+                }
+
+                // LA DATE
+                try {
+                    SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+                    java.util.Date   date = dateFormat.parse(((EditText)findViewById(R.id.etDate)).getText().toString());
+                }
+                catch (Exception e){
+                    Toast.makeText(getApplicationContext(), "Date invalide", Toast.LENGTH_SHORT).show();
+                    BDate = true;
+                }
+
+                // ### On Met Les Donnes
+                if (!BNum || !BNom || !BPre || !BAge || !BAdd || !BDate)
+                {
+                    try {
+                        // set some vars
+                        int num = Integer.parseInt(((EditText) findViewById(R.id.etNum)).getText().toString());
+                        String nom = ((EditText) findViewById(R.id.etNom)).getText().toString();
+                        String pre = ((EditText) findViewById(R.id.etPrenom)).getText().toString();
+                        int age = Integer.parseInt(((EditText) findViewById(R.id.etAge)).getText().toString());
+                        String add = ((EditText) findViewById(R.id.etAdd)).getText().toString();
+                        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+                        java.util.Date date = dateFormat.parse(((EditText) findViewById(R.id.etDate)).getText().toString());
+
+                        // add those vars
+                        vmapref.setNum(num);
+                        vmapref.setNom(nom);
+                        vmapref.setPrenom(pre);
+                        vmapref.setAge(age);
+                        vmapref.setAdresse(add);
+                        vmapref.setDatenaiss(date);
+                    }
+                    catch (Exception e){
+
+                    }
+                }
             }
         });
     }
