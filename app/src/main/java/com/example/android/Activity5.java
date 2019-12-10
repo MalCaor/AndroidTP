69bd509484cd73f5fb3b9670a4b621a5cfa56598
MalCaor <xav.lemen@orange.fr>
2019-12-10 16:10:04 +0100

    bool

--- app/src/main/java/com/example/android/Activity5.java
+++ app/src/main/java/com/example/android/Activity5.java
@@ -8,7 +8,11 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import android.content.SharedPreferences;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+import android.widget.TextView;
 public class Activity5 extends AppCompatActivity {
 
 
@@ -23,6 +27,45 @@ public class Activity5 extends AppCompatActivity {
         Gson gson = new GsonBuilder().setDateFormat("dd/MM/yyyy").create();
         eleve vmapref=gson.fromJson(vjson,eleve.class);
         Toast.makeText(getApplicationContext(),    vmapref.getNom()+" "+vmapref.getPrenom()+" "+new SimpleDateFormat("dd/MM/yyyy").format(vmapref.getDatenaiss()) ,Toast.LENGTH_LONG).show();
-        
+
+        // set the TextView
+        final TextView etNum    = (TextView)findViewById(R.id.etNum);
+        final TextView etNom    = (TextView)findViewById(R.id.etNom);
+        final TextView etPrenom = (TextView)findViewById(R.id.etPrenom);
+        final TextView etAge    = (TextView)findViewById(R.id.etAge);
+        final TextView etAdd    = (TextView)findViewById(R.id.etAdd);
+        final TextView etDate   = (TextView)findViewById(R.id.etDate);
+
+        // set the value of textView
+        etNum.setText(String.valueOf(vmapref.getNum()));
+        etNom.setText(vmapref.getNom());
+        etPrenom.setText(vmapref.getPrenom());
+        etAge.setText(String.valueOf(vmapref.getAge()));
+        etAdd.setText(vmapref.getAdresse());
+        etDate.setText(String.valueOf(vmapref.getAge()));
+
+        // get the button
+        Button Ok  = (Button)findViewById(R.id.btnOk);
+        Button Can = (Button)findViewById(R.id.btnCancel);
+
+        // Cancel BTN
+        Can.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                finish();
+
+            }
+        });
+
+        // OK btn
+        Ok.setOnClickListener(new View.OnClickListener() {
+            boolean BNum, BNom, BPre, BAge, BAdd, BDate;
+            BNum = BNom = BPre = BAge = BAdd = BDate = false;
+            public void onClick(View v) {
+                try {
+                    String num=((EditText)findViewById(R.id.etNum)).getText().toString();
+                }
+
+            }
+        });
     }
 }
